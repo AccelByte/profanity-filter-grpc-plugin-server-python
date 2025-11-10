3518593376d3c1d35b934c03640ebd818cca12a0
Elmer Nocon <elmernocon@gmail.com>
2025-11-10 14:21:02 +0800

    chore: add dev container support

--- src/profanityFilter_pb2.pyi
+++ src/profanityFilter_pb2.pyi
@@ -5,15 +5,15 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class ExtendProfanityValidationRequest(_message.Message):
-    __slots__ = ["userId", "value"]
-    USERID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("value", "userId")
     VALUE_FIELD_NUMBER: _ClassVar[int]
-    userId: str
+    USERID_FIELD_NUMBER: _ClassVar[int]
     value: str
+    userId: str
     def __init__(self, value: _Optional[str] = ..., userId: _Optional[str] = ...) -> None: ...
 
 class ExtendProfanityValidationResponse(_message.Message):
-    __slots__ = ["isProfane", "message"]
+    __slots__ = ("isProfane", "message")
     ISPROFANE_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     isProfane: bool
